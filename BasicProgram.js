303fd1f603685bc3305048bc2c9652ccb7d49086
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-24 09:07:31 +0530

    Added: Employee Wage Programm Added By JavaScript

--- BasicProgram.js
+++ BasicProgram.js
@@ -6,22 +6,20 @@ var totalWage=0;
 var storeData=new Map();
 var day=0;
 while(noOfHoursInMonth!=0 && noOfWorkingDayInMonth!=0){
-    
-var attendance=Math.floor(Math.random()*10);
-// fullTime
-if(attendance >= 5){
+    var attendance=Math.floor(Math.random()*10);
+    if(attendance >= 5){
     noOfHoursInMonth=noOfHoursInMonth-8;
     noOfWorkingDayInMonth=noOfWorkingDayInMonth-1;
     totalWage=fullTimeWage+totalWage;
     storeData.set(day,fullTimeWage);
-}
-else {
+    }
+    else {
     noOfHoursInMonth=noOfHoursInMonth-8;
     noOfWorkingDayInMonth=noOfWorkingDayInMonth-1;
     totalWage=partTimeWage+totalWage;
     storeData.set(day,partTimeWage);
-}
-day++;
+    }
+    day++;
 }
 console.log(storeData);
 console.log("Total Wage Of Employee is : "+totalWage)
