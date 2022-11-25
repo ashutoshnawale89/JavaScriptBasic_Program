84c2766da786ac94164602f5b2a63ffb7e9e139f
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-25 09:45:08 +0530

    Added:Regex Pattern To check The Pincode If space between Also true

--- RegexProblem.js
+++ RegexProblem.js
@@ -1,11 +1,12 @@
 const prompt=require("prompt-sync")({sigint:true});
-    let pinCodeRegex = /^[0-9A-Za-z]{6,}[^a-zA-Z]$/;
-    let pinCode = parseInt(prompt("Enter The pincode"));
+let pinCodeRegex = /^[0-9]{3}[ ]?[0-9]{3}$/;
+let pinCode = (prompt("Enter The pincode   "));
 
-    if(pinCodeRegex.test(pinCode))
-     {
-        console.log(true)
-     }else
-     {
-        console.log(false) 
-      }
+
+if(pinCodeRegex.test(pinCode))
+ {
+    console.log("true");
+ }else
+ {
+    console.log("false");
+  }
