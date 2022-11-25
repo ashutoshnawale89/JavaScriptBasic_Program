3b668740523968441505fdcc43f1b6969b4392fa
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-25 09:53:27 +0530

    Added:Regex Pattern To check The Email Pattern

--- RegexProblem.js
+++ RegexProblem.js
@@ -1,12 +1,20 @@
-const prompt=require("prompt-sync")({sigint:true});
-let pinCodeRegex = /^[0-9]{3}[ ]?[0-9]{3}$/;
-let pinCode = (prompt("Enter The pincode   "));
-
-
-if(pinCodeRegex.test(pinCode))
+function regecPattern(pattern){
+let pinCodeRegex = /^[a-zA-Z]+[.+-]?[A-Za-z0-9]*[@#$&][A-Za-z0-9]+[.+-][A-Za-z0-9]{2,}[.]*[A-Za-z]*$/;
+if(pinCodeRegex.test(pattern))
  {
     console.log("true");
  }else
  {
     console.log("false");
   }
+}
+regecPattern("abc@yahoo.com");
+regecPattern("abc-100@yahoo.com");
+
+regecPattern("abc.100@yahoo.com");
+regecPattern("abc111@abc.com");
+regecPattern("abc-100@abc.net");
+regecPattern("abc.100@abc.com.au");
+regecPattern("abc@1.com");
+regecPattern("abc@gmail.com.com");
+regecPattern("abc+100@gmail.com");
