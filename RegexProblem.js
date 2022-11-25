dc8d1901da5ee57aa8e4d7a8164f14b195d61d53
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-25 09:24:11 +0530

    Added:Regex Pattern To check The Pincode Without End Special Character

--- RegexProblem.js
+++ RegexProblem.js
@@ -1,5 +1,5 @@
 const prompt=require("prompt-sync")({sigint:true});
-    let pinCodeRegex = /^[A-Za-z]{1}[0-9]{6}$/;
+    let pinCodeRegex = /^[0-9A-Za-z]{6,}[^a-zA-Z]$/;
     let pinCode = parseInt(prompt("Enter The pincode"));
 
     if(pinCodeRegex.test(pinCode))
